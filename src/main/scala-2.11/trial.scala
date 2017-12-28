ff0ea23ba481b39c34e3a4e9c096853277d6bfab
Jaswant Jonnada <jaswanth04@gmail.com>
2017-12-28 11:24:52 +0530

    Changed Trial

--- src/main/scala-2.11/trial.scala
+++ src/main/scala-2.11/trial.scala
@@ -4,6 +4,17 @@ object trial {
 
   def main(args: Array[String]): Unit = {
 
+    val a = DenseMatrix((1,2),(3,4))
+    val b = DenseMatrix((4,5),(6,7))
+
+    println(a.t)
+    println("---------")
+    println(b.t)
+    println("----------")
+    println(a*b)
+    println("-------------")
+    println(a.t * b.t)
+
   }
 
 }
